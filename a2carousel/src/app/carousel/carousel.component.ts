39f67384d52e28321ec0ad50d50ed6382e6182e2
eplaza <enrique.plaza@objective-partner.com>
2017-03-08 19:55:12 +0100

    get getWidth and translateX

--- a2carousel/src/app/carousel/carousel.component.ts
+++ a2carousel/src/app/carousel/carousel.component.ts
@@ -9,31 +9,39 @@ import { CarouselItem } from '../shared/models/carousel-item.model'
 export class CarouselComponent implements OnInit, AfterViewInit  {
   
   @Input() public items:Array<CarouselItem>;
-   
+  
+  protected currentImageIndex: number;
   protected deltaX: number; 
   protected carousel_width: number;
-  @ViewChild('wraper') wraper: ElementRef;
+  protected translated_distance: number;
+  protected threshold: number;
+  @ViewChild('wraper') wraper: ElementRef; //we translate the wrapper layer
 
   constructor() { 
        
   }
 
   public ngOnInit(): void {
-    if(this.items && this.items.length) {
-
-    }
+    this.currentImageIndex = 0;
+    this.deltaX = 0;
+    this.translated_distance = 0;
   }
 
   ngAfterViewInit() {
-    // available here
      if(this.items && this.items.length > 0){
-       this.setWidth();
+       this.carousel_width = this.getWidth();
+       this.translated_distance = this.translated_distance - this.carousel_width;
+       this.translateX(this.translated_distance);
+       this.threshold = this.carousel_width * 0.4; 
      }
   } 
 
   protected setWidth(): void {
      this.carousel_width = this.items.length * 100;
      this.wraper.nativeElement.style('width', this.carousel_width+'%');
+  }
+  protected getWidth(): number {
+    return parseInt(window.getComputedStyle(this.wraper.nativeElement).width.replace(/[^-\d\.]/g, '10'));
   }
 
   protected onResize(): void {
@@ -59,7 +67,11 @@ export class CarouselComponent implements OnInit, AfterViewInit  {
       console.log("Touch End");
   }
 
+  public onTransitionEnd(event) {  
+  
+  }
+  
   protected translateX(distance: number): void {
-
+     this.wraper.nativeElement.style.transform = "translate3D(" + distance + "px,0,0)";
   }
 }
