0a53901fd3c5268fdc2266c721330fca393d1d93
eplaza <enrique.plaza@objective-partner.com>
2017-03-08 21:12:55 +0100

    deltaX, touchStard,  touchMove

--- a2carousel/src/app/carousel/carousel.component.ts
+++ a2carousel/src/app/carousel/carousel.component.ts
@@ -15,6 +15,8 @@ export class CarouselComponent implements OnInit, AfterViewInit  {
   protected carousel_width: number;
   protected translated_distance: number;
   protected threshold: number;
+  protected isTransitioning: boolean;
+  protected touchStartClientX: number; 
   @ViewChild('wraper') wraper: ElementRef; //we translate the wrapper layer
 
   constructor() { 
@@ -25,12 +27,14 @@ export class CarouselComponent implements OnInit, AfterViewInit  {
     this.currentImageIndex = 0;
     this.deltaX = 0;
     this.translated_distance = 0;
+    this.isTransitioning = false;
+    this.touchStartClientX = 0;
   }
 
   ngAfterViewInit() {
      if(this.items && this.items.length > 0){
        this.carousel_width = this.getWidth();
-       this.translated_distance = this.translated_distance - this.carousel_width;
+       this.translated_distance = this.translated_distance - this.carousel_width; 
        this.translateX(this.translated_distance);
        this.threshold = this.carousel_width * 0.4; 
      }
@@ -41,7 +45,7 @@ export class CarouselComponent implements OnInit, AfterViewInit  {
      this.wraper.nativeElement.style('width', this.carousel_width+'%');
   }
   protected getWidth(): number {
-    return parseInt(window.getComputedStyle(this.wraper.nativeElement).width.replace(/[^-\d\.]/g, '10'));
+    return parseInt(window.getComputedStyle(this.wraper.nativeElement).width.replace(/[^-\d\.]/g, ''));
   }
 
   protected onResize(): void {
@@ -49,29 +53,42 @@ export class CarouselComponent implements OnInit, AfterViewInit  {
   }
 
   public swipeLeft(): void {
-
+    this.isTransitioning = true;
   }
 
   public swipeRigth(): void {
+    this.isTransitioning = true;
 
   }
   public onTouchStart(event:any): void { 
       console.log(event);
+      this.touchStartClientX = event.changedTouches[0].clientX;
   }
 
   public onTouchMove(event:any): void{ 
-      console.log(event);
+    console.log(event);
+    this.wraper.nativeElement.style.transition = "none";
+    this.deltaX = this.getDeltaX(event);
+    this.translateX(this.deltaX + this.translated_distance);   
   }
   
   public onTouchEnd(event: any): void {
       console.log("Touch End");
+      this.translated_distance =  this.translated_distance + this.deltaX;
   }
 
   public onTransitionEnd(event) {  
-  
+    this.isTransitioning = false;
   }
-  
+
   protected translateX(distance: number): void {
      this.wraper.nativeElement.style.transform = "translate3D(" + distance + "px,0,0)";
+  }
+
+  protected getDeltaX(event: any): number{
+    if(event.changedTouches){
+      return event.changedTouches[0].clientX - this.touchStartClientX;
+    }
+    return 0;
   }
 }
